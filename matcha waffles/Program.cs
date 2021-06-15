362eca6919cc4de99caf04d8d3268b6ddad6ee33
Om Rajpal <omrajpal15@gmail.com>
2021-06-15 14:08:28 -0500

    changinges

--- matcha waffles/Program.cs
+++ matcha waffles/Program.cs
@@ -6,11 +6,7 @@ namespace matcha_waffles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("My name is Rohit. I will be attending BEHS next year. My favorite season is fall.");
-            Console.WriteLine("My name is Om. I will be attending BCHS next year. My favorite season was summer.");
-            Console.WriteLine("My name is Chitvan. I will be attending BCHS next year. My favorite season is fall.");
-            Console.WriteLine("My name is Tomski. I will be home schooled next year.My favorite season is fall.");
+        
         }
     }
 }
