7f2c8527ec80645284830c6e7f9a642c3573957c
Tomskeeeeee <hokiebadger@gmail.com>
2021-06-15 14:01:47 -0500

    added fav line

--- matcha waffles/Program.cs
+++ matcha waffles/Program.cs
@@ -10,6 +10,7 @@ namespace matcha_waffles
             Console.WriteLine("My name is Rohit. I will be attending BEHS next year. My favorite season is fall.");
             Console.WriteLine("My name is Om. I will be attending BCHS next year. My favorite season was summer.");
             Console.WriteLine("My name is Chitvan. I will be attending BCHS next year. My favorite season is fall.");
+            Console.WriteLine("My name is Tomski. I will be home schooled next year.My favorite season is fall.");
         }
     }
 }
